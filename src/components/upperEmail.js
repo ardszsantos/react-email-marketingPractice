64293204bf18733d81048f51bf468cd7409f10cd
Fredaobob <lokgamer01@gmail.com>
2024-08-26 12:32:54 -0300

    started the development!

---
+++ src/components/upperEmail.js
@@ -0,0 +1,14 @@
+import '../App.css';
+
+const UpperEmail = () => {
+  return ( 
+    <div className='imageContainer'>
+      <nav className="navBarContainer">
+        <img className="navBarLogo" src="logo.png" alt="logo"/>
+        <p className="navBarP">12 nov, 2021</p> 
+      </nav>
+    </div>
+  );
+}
+
+export default UpperEmail;
